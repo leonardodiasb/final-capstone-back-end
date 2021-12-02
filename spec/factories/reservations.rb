fdb5e1307dd30f11a3661d411c0fb1dd1d019302
d4nielj <d4niel.djm@gmail.com>
2021-12-02 13:10:56 -0500

    Fix linter errors

--- spec/factories/reservations.rb
+++
@@ -1,7 +0,0 @@
-FactoryBot.define do
-  factory :user do
-    first_name { Faker::Movies::StarWars.character.split.first }
-    last_name { Faker::Movies::StarWars.character.split.last }
-    email { "#{first_name}@starwars.com" }
-  end
-end
