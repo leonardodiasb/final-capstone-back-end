08500aad2a5656e40bdda323b0f0d9321b1b6ca0
d4nielj <d4niel.djm@gmail.com>
2021-12-02 15:10:18 -0500

    Add basic routes for endpoints

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  namespace :v1 do
+    resources :restaurants
+    resources :reservations
+    resources :shifts
+    resources :categories
+  end  
 end
