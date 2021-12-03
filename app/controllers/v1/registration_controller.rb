5b6a35e446c029717eb857f0d204ce24844663aa
Alonso Falconi <alonso.fl@live.com>
2021-12-03 09:59:51 -0500

    Handle registrations errors

--- app/controllers/v1/registration_controller.rb
+++ app/controllers/v1/registration_controller.rb
@@ -2,15 +2,22 @@ class V1::RegistrationController < ApplicationController
   skip_before_action :authenticate_request, only: [:create]
 
   def create
-    @user = User.new
-    @user.first_name = params[:first_name]
-    @user.last_name = params[:last_name]
-    @user.email = params[:email]
-    @user.password = params[:password]
-    @user.password_confirmation = params[:password_confirmation]
-    return unless @user.save!
+    @user = User.new(user_params)
+    return render json: @user.errors, status: :unprocessable_entity unless @user.save
 
     command = AuthenticateUser.call(params[:email], params[:password])
     render json: { auth_token: command.result } if command.success?
+  end
+
+  private
+
+  def user_params
+    params.permit(
+      :first_name,
+      :last_name,
+      :email,
+      :password,
+      :password_confirmation
+    )
   end
 end
