2a6bcd5e2857543f4f11713b52bf4aa717db3d65
Alonso Falconi <alonso.fl@live.com>
2021-12-03 08:03:44 -0500

    Add email in token response

--- app/commands/authenticate_user.rb
+++ app/commands/authenticate_user.rb
@@ -7,7 +7,7 @@ class AuthenticateUser
   end
 
   def call
-    JsonWebToken.encode(user_id: user.id) if user
+    JsonWebToken.encode(user_id: user.id, user_email: user.email) if user
   end
 
   private
