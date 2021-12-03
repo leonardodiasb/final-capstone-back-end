58be5b0edcc8519cbfcf621d70a341300ec88507
Alonso Falconi <alonso.fl@live.com>
2021-12-03 10:54:57 -0500

    Add secret_ley_base method

--- lib/json_web_token.rb
+++ lib/json_web_token.rb
@@ -7,13 +7,22 @@ class JsonWebToken
 
     def decode(token)
       p Rails.application.secret_key_base
-      p JWT.decode(token, Rails.application.secret_key_base)[0]
-      body = JWT.decode(token, Rails.application.secret_key_base)[0]
+      p JWT.decode(token, Rails.application.secret_key_base)
+      body = JWT.decode(token, Rails.application.secret_key_base)
       HashWithIndifferentAccess.new body
       p body
     rescue StandardError
       p 'error'
       nil
     end
+
+    def secret_key_base
+      if Rails.env.development? || Rails.env.test?
+        secrets.secret_key_base ||= generate_development_secret
+      else
+        validate_secret_key_base(
+          ENV["SECRET_KEY_BASE"] || credentials.secret_key_base || secrets.secret_key_base
+        )
+      end
   end
 end
