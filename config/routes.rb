ded3413472673b08c06c01baef4065372e98c38a
Alonso Falconi <alonso.fl@live.com>
2021-12-03 10:02:51 -0500

    Add demo route

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   namespace :v1, defaults: { format: 'json' } do
     post 'authenticate', to: 'authentication#authenticate'
     post 'register', to: 'registration#create'
+    get 'users', to: 'users#index'
   end
 end
