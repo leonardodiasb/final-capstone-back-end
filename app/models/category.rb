03a6e8bca7fab9a824ddf0fee5fc05ca76a5c363
Alonso Falconi <alonso.fl@live.com>
2021-12-06 13:41:24 -0500

    Add json_list method to render JSON

--- app/models/category.rb
+++ app/models/category.rb
@@ -2,4 +2,13 @@ class Category < ApplicationRecord
   has_and_belongs_to_many :restaurants
 
   validates :name, presence: true
+
+  def self.json_list
+    all.map do |cat|
+    {
+      id: cat.id,
+      name: cat.name
+    }
+  end
+end
 end
