c412121aa7232d147d061db38eaf5d6ecbcf5bdd
Alonso Falconi <alonso.fl@live.com>
2021-12-06 16:36:20 -0500

    Add create method in Shifts controller

--- app/controllers/v1/shifts_controller.rb
+++ app/controllers/v1/shifts_controller.rb
@@ -3,4 +3,15 @@ class V1::ShiftsController < ApplicationController
     @shifts = Shift.json_list
     render json: @shifts
   end
+
+  def create
+    if @current_user.role == 'admin'
+      @shift = Shift.new(name: params[:name])
+      return render json: @shift.errors, status: :unprocessable_entity unless @shift.save
+
+      render json: { status: :created, message: 'Shift created successfully.', body: @shift }, status: :created
+    else
+      render json: { error: 'Not Authorized' }, status: 401
+    end
+  end
 end
