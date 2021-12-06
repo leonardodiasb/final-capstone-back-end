18c4a1cdaf0d698347db34e1c0df9efa8c8a179b
Alonso Falconi <alonso.fl@live.com>
2021-12-06 15:51:42 -0500

    Add create method for categories controller

--- app/controllers/v1/categories_controller.rb
+++ app/controllers/v1/categories_controller.rb
@@ -3,4 +3,11 @@ class V1::CategoriesController < ApplicationController
     @categories = Category.json_list
     render json: @categories
   end
+
+  def create
+    @category = Category.new(name: params[:name])
+    return render json: @category.errors, status: :unprocessable_entity unless @category.save
+
+    render json: { status: :created, data: @category }
+  end
 end
