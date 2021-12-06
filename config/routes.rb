2bde93638278b34dcbb116c4dc71ecf356df1ca8
leonardo <leonardodias@ucl.br>
2021-12-06 14:43:33 -0300

    Create routes for restaurant

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
+  mount Rswag::Ui::Engine => '/api-docs'
+  mount Rswag::Api::Engine => '/api-docs'
   namespace :v1, defaults: { format: 'json' } do
     post 'authenticate', to: 'authentication#authenticate'
     post 'register', to: 'registration#create'
     get 'users', to: 'users#index'
+    resources :restaurants
   end
 end
