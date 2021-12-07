05ba8932eb19e04c8a816a1dbb790938239d1c71
leonardo <leonardodias@ucl.br>
2021-12-07 14:30:54 -0300

    Create spec tests for create restaurant and retrieve list of restaurants requests

--- spec/requests/api/restaurants_spec.rb
+++ spec/requests/api/restaurants_spec.rb
@@ -5,6 +5,7 @@ RSpec.describe 'api/restaurants', type: :request do
     post 'Creates a new Restaurant' do
       tags 'Restaurants'
       consumes 'application/json'
+      security [Bearer: {}]
       parameter name: :restaurant, in: :body, schema: {
         type: :object,
         properties: {
@@ -12,18 +13,28 @@ RSpec.describe 'api/restaurants', type: :request do
           description: { type: :string },
           reservation_spots: { type: :integer },
           image: { type: :string },
-          price_range: { type: :integer }
+          price_range: { type: :integer },
+          shifts: { type: :array },
+          categories: { type: :array }
         },
-        required: %w[name description reservation_spots image price_range]
+        required: %w[name description reservation_spots image price_range shifts categories]
       }
 
       response '201', 'Restaurant created' do
-        let(:restaurant) { { name: 'foo', description: 'bar', reservation_spots: 10, image: 'http://test.test', price_range: 3 } }
+        let(:shift) { create(:shift) }
+        let(:category) { create(:category) }
+        let(:user) { create(:user) }
+        let(:"Authorization") { "#{AuthenticateUser.call(user.email, user.password).result}" }
+        let(:restaurant) { { name: 'foo', description: 'bar', reservation_spots: 10, image: 'http://test.test', price_range: 3, shifts: [shift.id], categories: [category.id] } }
         run_test!
       end
 
-      response '422', 'invalid request' do
-        let(:restaurant) { { name: 'Foo' } }
+      response '400', 'bad request' do
+        let(:shift) { create(:shift) }
+        let(:category) { create(:category) }
+        let(:user) { create(:user) }
+        let(:"Authorization") { "#{AuthenticateUser.call(user.email, user.password).result}" }
+        let(:restaurant) { { name: 'Foo', shifts: [shift.id], categories: [category.id] } }
         run_test!
       end
     end
@@ -33,8 +44,11 @@ RSpec.describe 'api/restaurants', type: :request do
     get 'Retrieves a list of Restaurants' do
       tags 'Restaurants'
       produces 'application/json'
+      security [Bearer: {}]
 
       response '200', 'Restaurants found' do
+        let(:user) { create(:user) }
+        let(:"Authorization") { "#{AuthenticateUser.call(user.email, user.password).result}" }
         schema type: :array,
           properties: {
             id: { type: :integer },
@@ -42,7 +56,9 @@ RSpec.describe 'api/restaurants', type: :request do
             description: { type: :string },
             reservation_spots: { type: :integer },
             image: { type: :string },
-            price_range: { type: :integer }
+            price_range: { type: :integer },
+            shifts: { type: :array },
+            categories: { type: :array }
           }
         run_test!
       end
