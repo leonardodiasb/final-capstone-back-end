7bee9ad1774239a54212c27f2dfb0f4162d1e9b5
Alonso Falconi <alonso.fl@live.com>
2021-12-07 14:12:15 -0500

    Add destroy method to Reservations controller

--- app/controllers/v1/reservations_controller.rb
+++ app/controllers/v1/reservations_controller.rb
@@ -12,6 +12,13 @@ class V1::ReservationsController < ApplicationController
     render json: { status: :created, message: 'Reservation created successfully.', body: @reservation }, status: :ok
   end
 
+  def destroy
+    @reservation = Reservation.find(params[:id])
+    return render json: @reservation.errors, status: :unprocessable_entity unless @reservation.destroy
+
+    render json: { status: :no_content, message: 'Reservation deleted successfully.', body: nil }, status: :ok
+  end
+
   private
 
   def reservation_params
