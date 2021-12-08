b9588706be5d87948e4515be757bdac2e2f8b81c
Alonso Falconi <alonso.fl@live.com>
2021-12-08 12:26:38 -0500

    Handle date with no reservation in Restaurant model

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -15,18 +15,24 @@ class Restaurant < ApplicationRecord
       date_reservations.push(res) unless res.date != DateTime.parse(date)
     end
 
-    reservations_per_shift = {}
-    date_reservations.each do |res|
-      if reservations_per_shift.has_key?(res.shift)
-        reservations_per_shift[res.shift] += 1
-      else
-        reservations_per_shift[res.shift] = 1
-      end
-    end
-
     spots_avail_date = {}
-    shifts.each do |sh|
-      spots_avail_date[sh.name] = reservation_spots - reservations_per_shift[sh.name]
+    if date_reservations.empty?
+      shifts.each do |sh|
+        spots_avail_date[sh.name] = reservation_spots
+      end
+    else
+      reservations_per_shift = {}
+      date_reservations.each do |res|
+        if reservations_per_shift.has_key?(res.shift)
+          reservations_per_shift[res.shift] += 1
+        else
+          reservations_per_shift[res.shift] = 1
+        end
+      end
+
+      shifts.each do |sh|
+        spots_avail_date[sh.name] = reservation_spots - reservations_per_shift[sh.name]
+      end
     end
 
     spots_avail_date
