084b63944aea5dbc402e1714bb97b1734ff23595
Alonso Falconi <alonso.fl@live.com>
2021-12-08 11:27:20 -0500

    Add spots available per date method in restaurant model

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -8,6 +8,29 @@ class Restaurant < ApplicationRecord
   validates :reservation_spots, numericality: { greater_than_or_equal_to: 1 }
   validates :image, presence: true, url: true
 
+  def spots(date)
+    date_reservations = []
+    reservations.each do |res|
+      date_reservations.push(res) unless res.date != DateTime.parse(date)
+    end
+
+    reservations_per_shift = {}
+    date_reservations.each do |res|
+      if reservations_per_shift.has_key?(res.shift)
+        reservations_per_shift[res.shift] += 1
+      else
+        reservations_per_shift[res.shift] = 1
+      end
+    end
+
+    spots_avail_date = {}
+    shifts.each do |sh|
+      spots_avail_date[sh] = reservation_spots - reservations_per_shift[sh.name]
+    end
+
+    p spots_avail_date
+  end
+
   def self.json_list
     order(name: :asc)
       .includes(:shifts, :categories)
