37044a22db50821c6cc27e44ec0ec41d7a47b7ad
leonardo <leonardodias@ucl.br>
2021-12-14 19:03:06 -0300

    Create and fix category, reservation, shift and user model spec files

--- spec/models/reservation_spec.rb
+++ spec/models/reservation_spec.rb
@@ -1,5 +1,11 @@
 require 'rails_helper'
 
-RSpec.describe Category, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+RSpec.describe Reservation, type: :model do
+  let!(:user) { create(:user) }
+  let!(:restaurant) { create(:restaurant) }
+  let!(:reservation) { create(:reservation, user_id: user.id, restaurant_id: restaurant.id) }
+
+  it 'is a valid reservation' do
+    expect(reservation).to be_valid
+  end
 end
--- spec/models/shift_spec.rb
+++ spec/models/shift_spec.rb
@@ -1,5 +1,9 @@
 require 'rails_helper'
 
 RSpec.describe Shift, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let!(:shift) { create(:shift) }
+
+  it 'is a valid shift' do
+    expect(shift).to be_valid
+  end
 end
